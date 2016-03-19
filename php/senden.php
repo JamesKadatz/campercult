c05fc99a40b0dc153aeaed5560889ea77bc42f39
JamesKadatz <janneszk1@gmail.com>
2016-03-19 18:36:14 +0100

    kontaktformular

---
+++ php/senden.php
@@ -0,0 +1,13 @@
+<?php
+$empfaenger = "webmaster@beispiel.de"; //Hier die E-Mail
+$absendername = "Kontaktformular";
+$absendermail = $_POST['email'];
+$betreff = "Neue Anfrage";
+$text = "
+     Name: ".$_POST['name']."\n
+     E-Mail: ".$_POST['email']."\n
+     Nachricht: ".$_POST['nachricht']."\n";
+mail($empfaenger, $betreff, $text, "From: $absendername <$absendermail>");
+
+echo("Das Formular wurde erfolgreich versendet");
+?>
